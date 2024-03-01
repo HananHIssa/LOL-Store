0cc15867195414e6ba6bbc25d3da88458a67174a
HananHIssa <hananissa2018@gmail.com>
2024-03-01 19:08:46 +0200

    h

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar.jsx'
 import { Outlet } from 'react-router-dom';
 export default function root() {
   return (
-    <><Navbar /><Outlet /><footer /></>
+    <><Navbar /><Outlet /></>
   );
 }
 
