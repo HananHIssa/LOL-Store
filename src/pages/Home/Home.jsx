d5267dcba9db4699f2692bc5482a2f1df074e867
HananHIssa <hananissa2018@gmail.com>
2024-03-01 22:57:27 +0200

    final

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,39 @@
-import React, { useEffect,useState } from 'react'
-let [CategoryCard , setCategotyCards]=useState([]);
-const getData = async () =>{
-const {data} =await axios .get('https://ecommerce-node4.vercel.app/categories/active?page=1&limit=7');
-setCategotyCards(data.categories);
-console.log(data.categories);
-}
-useEffect(()=> {
-    getData();
-},[]);
+import React, { useEffect, useState } from 'react'
+import './Home.css'
+import axios from 'axios';
+
 function Home() {
+
+
+  let [CategoryCard, setCategotyCards] = useState([]);
+
+  const getData = async () => {
+ 
+      const { data } = await axios.get("https://ecommerce-node4.vercel.app/categories/active?page=1&limit=9#");
+      setCategotyCards(data.categories);
+      console.log(data.categories);
+  
+  }
+
+
+  useEffect(() => {
+    getData();
+
+  }, []);
+
   return (
     <>
-    CategoryCard.map( categorie => {
-       <div><h2>{categorie.name}</h2> </div>
-
-      }
-    
-    )
-    </>
+    <div className="parent">
+          {
+            CategoryCard.map(category =>
+              <div key={category.id}> 
+                  <img src={category.image.secure_url} />
+                  <h2>{category.name}</h2>
+               </div>
+            )
+          }
+     </div>
+  </>
   )
 }
 
