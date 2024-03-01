70da9d6b09d61677c7bbef62dcc60153afb2a06d
HananHIssa <hananissa2018@gmail.com>
2024-03-01 21:36:48 +0200

    l

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect,useState } from 'react'
 let [CategoryCard , setCategotyCards]=useState([]);
+const getData = async () =>{
 const {data} =await axios .get('https://ecommerce-node4.vercel.app/categories/active?page=1&limit=7');
 setCategotyCards(data.categories);
 console.log(data.categories);
+}
 useEffect(()=> {
     getData();
 },[]);
