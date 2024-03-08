913438b44125a01d4c117201364e0b0103cd07b2
HananHIssa <hananissa2018@gmail.com>
2024-03-08 04:25:29 +0200

    add signUp and SignIn page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
 
-function SignIn() {
+import React, { useState } from 'react';
+import './SignIn.css'
+const SignIn   = () => {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Login data submitted:', loginData);
+  };
+
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="container">
+      <div className='cont'>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={loginData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-export default SignIn
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
-
+import { useState } from 'react';
+import './SignUp.css'
 function SignUp() {
+  const [user,setUser] =useState({
+    userName:'',
+    email:'',
+    password:'',
+    image:'',
+  }
+  );
+  const handleChange = (e)=>{
+    const {name,value}=e.target;
+    setUser({
+      ...user,
+      [name]:value
+    });
+  };
+  const handleSumbit = (e)=>{
+    e.preventDefult();
+
+  };
   return (
-    <div>SignUp</div>
-  )
+    <div className='container'>
+      <div className="cont">
+        <form onSubmit={handleSumbit}>
+          <div className="form-group">
+            <label className="label">User Name</label>
+            <input
+              type="text"
+              value={user.userName}
+              name="userName"
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+  
+          <div className="form-group">
+            <label className="label">Email</label>
+            <input
+              type="text"
+              value={user.email}
+              name="email"
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+  
+          <div className="form-group">
+            <label className="label">Password</label>
+            <input
+              type="text"
+              value={user.password}
+              name="password"
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+  
+          <div className="form-group">
+            <label className="label">Image</label>
+            <input
+              type="file"
+              value={user.image}
+              name="image"
+              onChange={handleChange}
+              className="input"
+            />
+          </div>
+  
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
+      </div>
+    );
+
 }
 
 export default SignUp
